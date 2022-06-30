3d7196df0086d14b7c9680e7ebed30b64c223385
18God <1226343906@qq.com>
2022-06-30 21:07:42 +0800

    买书更新

---
+++ vue_day03/src/09.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="(item, index) in arr" :key="index" sytle="display:inline-block">
+      <input type="checkbox" value="item" v-model="ck"/>
+      <span>{{ item }}</span>
+    </div>
+    
+    <p>选中的元素，累计的值和为：{{}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+
+    };
+  },
+
+  
+};
+</script>
+
+<style></style>
