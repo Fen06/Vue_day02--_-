226c666ea14f2f1f3aed849eea8984eb1e655586
18God <1226343906@qq.com>
2022-07-03 20:39:45 +0800

    添加基本样式完成了按键新增

--- vue_day05/src/App.vue
+++ vue_day05/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <TodoHeader></TodoHeader>
-    <TodoMain></TodoMain>
+    <TodoHeader @down="down"></TodoHeader>
+    <TodoMain :list="list"></TodoMain>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -11,10 +11,32 @@ import TodoFooter from './components/TodoFooter.vue';
 import TodoMain from './components/TodoMain.vue';
 import TodoHeader from './components/TodoHeader.vue';
 export default {
+  data() {
+    return {
+      list: [
+        { id: 100, name: '吃饭', isDone: true },
+        { id: 201, name: '睡觉', isDone: false },
+        { id: 103, name: '打豆豆', isDone: true },
+      ],
+    };
+  },
   components: {
     TodoFooter,
     TodoMain,
     TodoHeader,
+  },
+
+  methods: {
+    down(val) {
+      const id = this.list[this.list.length - 1]
+        ? this.list[this.list.length - 1].id + 1
+        : 100;
+      this.list.push({
+        id,
+        name: val,
+        isDone: false,
+      });
+    },
   },
 };
 </script>
