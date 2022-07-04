0847d5096f638024a799f065d5c6d4e7ff3c284d
18God <1226343906@qq.com>
2022-07-04 20:55:47 +0800

    页面初始化渲染数据

---
+++ vue_day06/src/components/BOOKMIN.vue
@@ -0,0 +1,25 @@
+<template>
+  <tr>
+    <td>{{ info.id }}</td>
+    <td>{{ info.bookname }}</td>
+    <td>{{ info.author }}</td>
+    <td>{{ info.publisher }}</td>
+    <td><a href="javascript:;">删除</a><a href="javascript:;">详情</a></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    info: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
