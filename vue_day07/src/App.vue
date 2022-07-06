4d4a7ea4c161aee3d4514136e17ff506deb2e18a
18God <1226343906@qq.com>
2022-07-06 20:32:24 +0800

    初始化渲染表单

--- vue_day07/src/App.vue
+++ vue_day07/src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="box">
-    <UserPhoto></UserPhoto>
-    <Userpass></Userpass>
+  <div>
+    <Table></Table>
   </div>
 </template>
 
 <script>
-import UserPhoto from './components/UserPhoto.vue';
-import Userpass from './components/Userpassword.vue';
+import Table from './components/渲染表单数据/Table.vue';
 export default {
   components: {
-    UserPhoto,
-    Userpass,
+    Table,
   },
 };
 </script>
 
-<style scoped>
-body {
-  background-color: #f7f8f9;
-}
-.box {
-  width: 400px;
-  height: 600px;
-  background-color: #eff3f8;
-  margin: 200px auto;
-  /* box-shadow: 2px 2px #000 2px; */
-}
-</style>
+<style></style>
---
+++ vue_day07/src/components/渲染表单数据/Table.vue
@@ -0,0 +1,26 @@
+<template>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>序号</th>
+        <th>姓名</th>
+        <th>年龄</th>
+        <th>头像</th>
+      </tr>
+    </thead>
+    <thead>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </thead>
+  </table>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
