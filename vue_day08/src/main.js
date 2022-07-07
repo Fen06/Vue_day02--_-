e021f038d1d1fd1fe3ebddde35bc157d541513c3
18God <1226343906@qq.com>
2022-07-07 19:33:15 +0800

    数据加载+模块切换

--- vue_day08/src/main.js
+++ vue_day08/src/main.js
@@ -6,6 +6,10 @@ Vue.config.productionTip = false;
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/fonts/iconfont.css';
 
+import axios from 'axios';
+axios.defaults.baseURL = ' https://www.escook.cn';
+Vue.prototype.$axios = axios;
+
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
--- vue_day08/src/view/MyGoodsList.vue
+++ vue_day08/src/view/MyGoodsList.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1>MyGoodsList</h1>
+    <MyTable :arr="list"></MyTable>
   </div>
 </template>
 
 <script>
-export default {};
+import MyTable from '../components/MyTable.vue';
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+
+  components: {
+    MyTable,
+  },
+
+  created() {
+    this.$axios({
+      url: '/api/goods',
+    }).then((res) => {
+      console.log(res);
+      this.list = res.data.data;
+    });
+  },
+};
 </script>
 
 <style></style>
